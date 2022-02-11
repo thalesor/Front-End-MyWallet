527c135a2dacd15788e9b8cc90ab6a1d0e1fd660
Thales Ruano <thalesoruano@gmail.com>
2022-02-11 18:55:40 -0300

    chore deploy to heroku

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 // USERS
 async function getUser(data) 
 {
-    const requisitionObj = await axios.post("http://localhost:5000/user/signin", data);
+    const requisitionObj = await axios.post("https://back-wallet.herokuapp.com/user/signin", data);
     return requisitionObj;
 }
 
 async function insertUser(data) 
 {
-    const requisitionObj = await axios.post("http://localhost:5000/user/signup", data);
+    const requisitionObj = await axios.post("https://back-wallet.herokuapp.com/user/signup", data);
     return requisitionObj;
 }
 
 // REGISTRIES
 async function getRegistries(token) 
 {
-    const requisitionObj = await axios.get("http://localhost:5000/registry", {
+    const requisitionObj = await axios.get("https://back-wallet.herokuapp.com/registry", {
         headers: {
             Authorization: `Bearer ${token}`
           }
@@ -27,14 +27,14 @@ async function getRegistries(token)
 async function insertRegistry(data, token) 
 {
     data.Authorization = token;
-    const requisitionObj = await axios.post("http://localhost:5000/registry", data
+    const requisitionObj = await axios.post("https://back-wallet.herokuapp.com/registry", data
     );
     return requisitionObj;
 }
 
 async function deleteSession(token)
 {
-    const requisitionObj = await axios.delete("http://localhost:5000/session", {
+    const requisitionObj = await axios.delete("https://back-wallet.herokuapp.com/session", {
         headers: {
             token: `${token}`
           }
